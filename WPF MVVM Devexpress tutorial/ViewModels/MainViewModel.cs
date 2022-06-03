d5d54323651d8c4558b50cb94207fcb49724a9d4
truyen.lv <truyen.lv@THTC188>
2022-06-03 23:41:18 +0700

    tao command

--- WPF MVVM Devexpress tutorial/ViewModels/MainViewModel.cs
+++ WPF MVVM Devexpress tutorial/ViewModels/MainViewModel.cs
@@ -8,12 +8,32 @@ namespace WPF_MVVM_Devexpress_tutorial.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Entity
         NorthwindEntities db = new NorthwindEntities();
+        #endregion
+        #region List
         public ObservableCollection<Product> List_Product
         {
             get { return GetProperty(() => List_Product); }
             set { SetProperty(() => List_Product, value); }
         }
+        #endregion
+        #region Khai bao bien
+        public string ProductName
+        {
+            get { return GetProperty(() => ProductName); }
+            set { SetProperty(() => ProductName, value); }
+        }
+        #endregion
+        #region Phan Quyen
+        public bool cmdThem
+        {
+            get { return GetProperty(() => cmdThem); }
+            set { SetProperty(() => cmdThem, value); }
+        }
+    
+        #endregion
+
         protected override void OnInitializeInDesignMode()
         {
             base.OnInitializeInDesignMode();
@@ -22,14 +42,22 @@ namespace WPF_MVVM_Devexpress_tutorial.ViewModels
         protected override void OnInitializeInRuntime()
         {
             base.OnInitializeInRuntime();
-            Load_Product();
+            
 
         }
-
+        #region Khai bao command
+        public DelegateCommand Btn_LoadDS_SP { get; private set; }
+        #endregion
         public MainViewModel()
         {
-
+            Btn_LoadDS_SP  = new DelegateCommand(Btn_LoadDS_SP_, true);
         }
+
+        private void Btn_LoadDS_SP_()
+        {
+            Load_Product();
+        }
+
         public void Load_Product()
         {
             var list = db.Products.ToList();
